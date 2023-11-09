5f147e48bcf2637da2f993180b2f51ad005249a5
Dwiden <70680351+Dwidenbrahma@users.noreply.github.com>
2023-11-09 14:39:18 +0530

    first commit
    css static web

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// let changeButton = document.getElementById("bt");
+// let lightButton = document.getElementById("bt2");
+// let bodyColor = document.querySelector("body");
+
+
+// changeButton.addEventListener('click', ()=>{
+
+
+//     bodyColor.style.backgroundColor = "black";
+//     bodyColor.style.color = "#fff";
+//     changeButton.style.display = "none";
+
+
+// });
+
+// lightButton.addEventListener("click", ()=>{
+
+
+//     lightButton.style.display = "block";
+//     bodyColor.style.backgroundColor = "#FFFBF5";
+//     bodyColor.style.color = "#000"
+
+// });
